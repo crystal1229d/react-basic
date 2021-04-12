491652d4c23e0c719bb49b4cfa68fc52d05dc364
crystal1229d <crystal1229d@naver.com>
2021-04-13 01:21:53 +0900

    init project (template)

---
+++ template/src/app.js
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        header
+      </header>
+    </div>
+  );
+}
+
+export default App;
