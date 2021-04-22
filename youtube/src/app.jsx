51ce566c349a5116f17af5c54a3d2d7a1c6314de
crystal1229d <crystal1229d@naver.com>
2021-04-23 01:29:43 +0900

    make video item

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  const [videos, setViedos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     // 컴포넌트(의 state나 prop)가 업데이트나 마운트 될 때 마다 반복적으로 콜백함수가 호출된다
@@ -10,8 +11,22 @@ function App() {
     // 만약 videos가 업데이트된 경우에도 호출하고 싶다면 [videos]를 인자로 준다.
     // [name, videos] 와 같이 원하는 변수의 목록을 콜백함수의 인자로 전달할 수 있다.
     console.log('useEffect');
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAGSff-g2uYBvqpIdrUSijYV5G-jjJXNV0"
+      ,requestOptions
+    )
+      .then(response => response.json())  // response.text(), resonse.json()
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error))
+
   }, []);
-  return <h1>Hello</h1>;
+  return <VideoList videos={videos} />
 }
 
 export default App;
