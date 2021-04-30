c06d2e63aed8115064cb81382416bdcf3891336a
crystal1229d <crystal1229d@naver.com>
2021-05-01 01:14:27 +0900

    add youtube service

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -3,21 +3,12 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAGSff-g2uYBvqpIdrUSijYV5G-jjJXNV0`
-      , requestOptions
-    )
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
@@ -25,17 +16,9 @@ function App() {
     // 컴포넌트가 업데이트될 때마다 네트워크 통신을 하는 것은 성능에 좋지 않으므로 [] (빈 배열)을 두 번째 인자로 전달함으로써 마운트됐을 때만 (한 번만)호출된다
     // 만약 videos가 업데이트된 경우에도 호출하고 싶다면 [videos]를 인자로 준다.
     // [name, videos] 와 같이 원하는 변수의 목록을 콜백함수의 인자로 전달할 수 있다.
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAGSff-g2uYBvqpIdrUSijYV5G-jjJXNV0"
-      ,requestOptions
-    )
-      .then(response => response.json())  // response.text(), resonse.json()
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error))
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
 
   }, []);
   return (
